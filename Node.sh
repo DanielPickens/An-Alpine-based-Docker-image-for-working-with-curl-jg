6414e02a63ed4f7ae5277da386564b71ff72713a
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-01-31 01:46:57 -0700

    Create Node.sh

---
+++ Node.sh
@@ -0,0 +1,16 @@
+An Alpine-based Docker image for working with both curl and jq.
+
+Usage
+Both of the following docker run variations will succeed.
+
+Using jq as the Docker entrypoint
+docker run \
+  --rm \
+  -v $(pwd):/home \
+  Daniel/node-curl-jq '.[]' /home/test.json
+Providing a command to the container
+docker run \
+  --rm \
+  -v $(pwd):/home \
+  Daniel/node-curl-jq \
+  /bin/sh -c "cat /home/test.json | jq '.[]'"
